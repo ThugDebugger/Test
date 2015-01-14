16ebbbc59ed30c18722e6e06be373437f7a5f831
Justin E. Samuels <js1599.msu@gmail.com>
2015-01-14 16:53:18 -0600

    Fixed Multiple Issues (See disc)
    1) Twitter Bootstrap is now loaded via the CDN
    2) Updated the UX/UI to only having 2 buttons (Submit and Start Over)
    which when "Submit" is clicked, the values submitted are populated below
    the <hr> tag for the user to see and when "Start Over" is clicked, all
    services are cleared from memory in addition to the screen below the
    <hr> tag is cleared.
    
    3) The text input boxes are automatically cleared after a submission and
    the focus of the cursor returns to the first box
    
    4) Fixed issue where users were allowed to remove services from the base
    rate which they never added

--- test.js
+++ test.js
@@ -10,6 +10,9 @@ var services = [];
 //Array used to store the rates of services
 var rates = [];
 
+//Array used to store the unique key identifier of the serivces added by the user (prevents user from removing services never added)
+var values = [];
+
 //Static starting rate of $200 init
 var currentRate = 200;
 
@@ -22,7 +25,7 @@ function CurrentRate()
 
 //Function used to store the values entered into textboxes from test.html and perform error checking    
 function  Store()
-{   
+{  
     //gets the value from the service name textbox
     var p = document.getElementById("1").value;
 
@@ -45,6 +48,8 @@ function  Store()
       //push the value of p onto the services stack
       services.push(p);
 
+      
+
       //push the values of q onto the rates stack
       rates.push(q);
     }
@@ -61,16 +66,13 @@ function  Store()
         document.getElementById("01").innerHTML = "Numerical values only (0-9) !";
     }
 
-};
-
-//Function used to display current service offered below the <hr> tag in test.html
-function Display ()
-{
    //counter variable init
    var i = 0;
 
    //rate variable init
    var rate = 0;
+
+
 
    //Decision statement ONLY executes if there are no values in the services array
    if (services.length == 0)
@@ -88,25 +90,38 @@ function Display ()
        {    
           rate = rates[i];
 
+
           //appends the service and value to the <div> tag with the class of wrap, 
           //if the 'add service' link is clicked, the increaseRate function will be called with the variable 'rate' being passed
           //if the 'decreaseRate service' link is clicked, the decreaseRate function will be called with the variable 'rate' being passed 
-          $(".wrap").append(entry,': $', rate, ' ', '<br><a href = "#" id ="box'+i+'" value = "'+rate+'" onclick = "increaseRate('+rate+')">Add Service  </a><br>', '  ','<a href = "#" onclick = "decreaseRate('+rate+');">  Remove Service</a><br>', ' <br>');
+          //"i" is used as the unique identifier amoong the services, and is passed here as well to the increaseRate and decreaseRate functions to later be stored in the "values" array
+          $(".wrap").append(entry,': $', rate, ' ', '<br><a href = "#" id ="box'+i+'" value = "'+rate+'" onclick = "increaseRate('+rate+','+i+')">Add Service  </a><br>', '  ','<a href = "#" onclick = "decreaseRate('+rate+','+i+');">  Remove Service</a><br>', ' <br>');
             i++;
         });
     }
+    //Clear the Services textbox once user submitted data
+    document.getElementById("1").value = null;
+
+    //Clear the Rate textbox once user submitted data
+    document.getElementById("2").value = null;
+
+    //Move the users cursor back to the services textbox to allow for more input easily 
+    document.getElementById("1").focus();
 };
 
-//Function used to add rate values to the currentRate variable, responds to the 'add service' tag when clicked
-function increaseRate (rate)
+//Function used to add rate values to the currentRate variable, responds to the 'add service' tag when clicked, and pass the "i" counter variable number to the "values" array
+function increaseRate (rate,value)
 {  
+    
     var check = rate + currentRate;
 
-            //error check to not allow negative final values
+            //error check to not allow negative final values and to allow the service to the "values" array
             if (check >= 0)
             {
+            values.push(value);
             currentRate += rate;
             document.getElementById("Newrate").innerHTML =currentRate;    
+            
             }
             else
             {
@@ -115,9 +130,43 @@ function increaseRate (rate)
             }      
 };
 
-//Function used to subtract rate values from the currentRate variable, responds to the 'decreaseRate service' tag when clicked
-function decreaseRate(rate)
+//Function used to subtract rate values from the currentRate variable, responds to the 'decreaseRate service' tag when clicked, and pass the "i" counter variable number to the "values" array
+function decreaseRate(rate,value)
 {
+    //counter variable init
+    var j = 0;
+
+    //dog variable is init with the paramater "value" (dog is a random variable name, apart of my signature)
+    var dog = value;
+
+    // val is init to null 
+    var val = null;
+
+    //set the sentinel flag to false to begin, will change to true if the serivce value is found in the "values" array
+    var flag = false;
+
+    //loops through the "values" array which holds the list of services the user already added to their serivce
+    values.forEach(function(entry)
+    {
+        val = values[j]
+
+        //if the service being removed is found to be already added, the sentinel flag will be set to true
+        if (val == dog)
+        {
+            flag = true;
+        }
+        j++;
+    });
+
+    //if the sentinel flag is still set to flag, this decision structure will execute
+    if (flag == false)
+    {
+        alert("That service was never added");
+    }
+
+    //if the the sentinel flag is set to true (the value was found to be added by the user), all code below will execute
+    else
+    {
     //if the rate is negative, it will then be multiplied by negative 1 to ensure proper subtraction afterwards   
     if(rate < 0)
     {
@@ -125,17 +174,19 @@ function decreaseRate(rate)
     }
     var check = currentRate - rate;
 
-    //error check to not allow negative final values
+    //error check to not allow negative final values, and remove the serivce from the "values" array
     if(check >= 0)
     {
        currentRate -= rate;
        document.getElementById("Newrate").innerHTML =currentRate; 
+       values.pop(value);
     }
     else
     {
        //alert the user of the error
-        window.alert("Negative rates not allowed");
+        window.alert("Negative rates not allowed or you never added the service");
     }
+}
 };
  
 //Function used to empty both the service and rates arrays and update the Newrate value to ZERO 
@@ -151,12 +202,5 @@ function ClearRates()
     //update the Newrate value being displayed onto the screen
     document.getElementById("Newrate").innerHTML = "0";
     $(".wrap").empty();
-};
-
-// Function used to clear the screen of available services and their prices    
-function ClearScreen()
-{
-    //empties the <div> tag with a class of wrap in test.html
-   $(".wrap").empty();
 };
 
